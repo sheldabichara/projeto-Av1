c1568095c9ca1bab00c70f6b1832f7cbaf8dd3a5
sheldabichara <131830120+sheldabichara@users.noreply.github.com>
2023-04-26 00:26:58 -0300

    Add files via upload

---
+++ index (3).php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/portal_av/css/style_home.css">
+	<title>Admin</title>
+</head>
+<body>
+
+	<header>
+
+		<div class="container">
+
+			<div class="logo">
+				<img src="/portal_av/img/logo.png">
+				<h3>Encontre
+					<div><h3>Veículos</h3></div>
+				</h3>
+			</div>
+
+			<div class="menu_section">
+				<div class="menu_toggle">
+					
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<a href="#">LOGIN</a>
+						</li>
+					</ul>
+				</nav>			
+			</div>
+		</div>
+	</header>
+
+	<div>
+    	<p>Aproveite suas ferias com seguança</p>
+    	<h1>Alugue seu carro aqui</h1>
+	</div>
+
+	<div class="slider">
+
+		<div class="slides">
+			<!-- Butões de Radio -->
+			<input type="radio" name="radio-btn" id="radio1">
+			<input type="radio" name="radio-btn" id="radio2">
+			<input type="radio" name="radio-btn" id="radio3">
+			<input type="radio" name="radio-btn" id="radio4">
+			<input type="radio" name="radio-btn" id="radio5">
+			<input type="radio" name="radio-btn" id="radio6">
+			<!-- Fim Butões de Radio -->
+
+			<!-- Imagens do Slide -->
+			<div class="slide first">
+				<img src="/portal_av/img/carro1.png" alt="Imagem 1">
+			</div>
+			<div class="slide">
+				<img src="/portal_av/img/carro2.png" alt="Imagem 2">
+			</div>
+			<div class="slide ">
+				<img src="/portal_av/img/carro3.png" alt="Imagem 3">
+			</div>
+			<div class="slide ">
+				<img src="/portal_av/img/carro4.png" alt="Imagem 4">
+			</div>
+			<div class="slide ">
+				<img src="/portal_av/img/carro5.png" alt="Imagem 5">
+			</div>
+			<div class="slide ">
+				<img src="/portal_av/img/carro6.png" alt="Imagem 6">
+			</div>
+			<!--Fim das imagens-->
+
+			<!-- Navegação automatica -->
+			<div class="navigation-auto">
+				<div class="auto-btn1"></div>
+				<div class="auto-btn2"></div>
+				<div class="auto-btn3"></div>
+				<div class="auto-btn4"></div>
+				<div class="auto-btn5"></div>
+				<div class="auto-btn6"></div>
+			</div>
+			<!--Fim da navegação automatica-->
+
+		</div>
+
+		<div class="manual-navigation">
+			<label for="radio1" class="manual-btn"></label>
+			<label for="radio2" class="manual-btn"></label>
+			<label for="radio3" class="manual-btn"></label>
+			<label for="radio4" class="manual-btn"></label>
+			<label for="radio5" class="manual-btn"></label>
+			<label for="radio6" class="manual-btn"></label>
+		</div>
+
+	</div>
+
+	<div class="lista">
+		
+		<div class="tabela_lista">
+
+			<div class="texto">
+				<h2>Lista de carros disponiveis</h2>
+			</div>
+			
+			<div class="tabela first">
+				
+				<img src="/portal_av/img/carro1.png" alt="Imagem 1">
+
+				<div class="info_tabela">
+					<div class="descricao_vcl">Descrição: </div>
+					<div class="marca_vcl">Marca: </div>
+					<div class="codigo_vcl">Codigo: </div>
+					<div class="modelo_vcl">Modelo: </div>
+					<div class="ano_vcl">Ano: </div>
+				</div>
+
+			</div>
+
+			<div class="tabela first">
+				
+				<img src="/portal_av/img/carro1.png" alt="Imagem 1">
+
+				<div class="info_tabela">
+					<div class="descricao_vcl">Descrição: </div>
+					<div class="marca_vcl">Marca: </div>
+					<div class="codigo_vcl">Codigo: </div>
+					<div class="modelo_vcl">Modelo: </div>
+					<div class="ano_vcl">Ano: </div>
+				</div>
+
+			</div>
+
+			<div class="tabela first">
+				
+				<img src="/portal_av/img/carro1.png" alt="Imagem 1">
+
+				<div class="info_tabela">
+					<div class="descricao_vcl">Descrição: </div>
+					<div class="marca_vcl">Marca: </div>
+					<div class="codigo_vcl">Codigo: </div>
+					<div class="modelo_vcl">Modelo: </div>
+					<div class="ano_vcl">Ano: </div>
+				</div>
+
+			</div>
+
+
+			
+
+		</div>
+
+	</div>
+
+	<div class="pezinho"></div>
+
+	<script src="/portal_av/script.js"></script>
+
+</body>
+
+</html>
